d80368682f780a6e2dd90781c69f738da612eb33
Muhammad Saqib Khan <muhammad.saqib@avanza.pk>
2025-08-11 19:18:06 +0500

    modified login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import axios from '../api/axios';
+import '../styles/login.css'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   const [form, setForm] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
@@ -24,19 +26,41 @@ const Login = () => {
 
   return (
     <div className="login-container">
+      <div className='login-box'>
         <form onSubmit={handleSubmit}>
             <h2>Login</h2>
             <input placeholder="Email" value={form.email}
                     onChange={(e) => setForm({ ...form, email: e.target.value })} />
-            <input type="password" placeholder="Password" value={form.password}
+            {/* <input type={showPassword ? "text" : "password"} placeholder="Password" value={form.password}
                     onChange={(e) => setForm({ ...form, password: e.target.value })} />
+                    <span
+                        className="toggle-password"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? "👁️" : "🙈"}
+                    </span> */}
+            <div className="password-wrapper">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+              />
+              <span
+                className="toggle-password"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? "👁️" : "🙈"}
+              </span>
+            </div>
             <button type="submit">Login</button>
             {error && <p style={{color: 'red'}}>{error}</p>}
+            <p className='register-text'>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
         </form>
-        <p>
-            Don't have an account? <Link to="/register">Register</Link>
-        </p>
-    
+        
+      </div>
     </div>
 
   );
